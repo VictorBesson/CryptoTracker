e2806835227ce5a219803e4464621254b4f73226
Victor Besson <victor.besson@gmail.com>
2021-09-27 21:08:56 +0200

    getting api data in / route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const axios = require("axios");
+
+//init request param
+const requestOption = {
+  method: "GET",
+  url: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+  headers: {
+    "X-CMC_PRO_API_KEY": process.env.API_KEY,
+  },
+};
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  let crypto;
+  axios(requestOption)
+    .then((response) => {
+      console.log(response.data);
+      crypto = response.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  res.render("index", { title: "Express", crypto: crypto });
 });
 
 module.exports = router;
